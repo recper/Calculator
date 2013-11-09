e5ee1cbf15b6d7f5cf2bd89371bba6b567a9221b
recper <recperr@gmail.com>
2013-11-09 09:10:29 +0200

    Project is created

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Сергей
+ * Date: 09.11.13
+ * Time: 8:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class Calculator {
+    public static void main(String args[]){
+        System.out.print(1);
+    }
+}
